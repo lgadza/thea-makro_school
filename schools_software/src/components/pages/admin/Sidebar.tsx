42d49ca93d2f50d649405f9347ba5ebd9d622373
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-04-26 12:00:01 +0200

    good

--- schools_software/src/components/pages/admin/Sidebar.tsx
+++ schools_software/src/components/pages/admin/Sidebar.tsx
@@ -1,89 +1,11 @@
-// import { useState } from 'react';
-// import { Container, Row, Col, Navbar, Nav, ListGroup } from 'react-bootstrap';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHome, faUserGraduate, faLevelUpAlt, faChalkboardTeacher, faUsers, faBook, faBed, faFileAlt, faIdCard,faBookOpen,faClipboard } from '@fortawesome/free-solid-svg-icons';
-// import { Link } from 'react-router-dom';
 
-// const AdminSidebarMenu = (): JSX.Element  => {
-//   const [showMenu, setShowMenu] = useState(false);
+import { faBars, faHome, faUserGraduate, faLevelUpAlt, faChalkboardTeacher, faUsers, faBook, faBed, faFileAlt,faInfo, faIdCard,faBookOpen,faClipboard } from '@fortawesome/free-solid-svg-icons';
 
-//   const toggleMenu = () => {
-//     setShowMenu(!showMenu);
-//   };
-
-//   return (
-    
-//       <Container fluid>
-//         <Row>
-//           <Col sm={2} id="sidebar-menu" className={showMenu ? "active" : ""}>
-//             <Nav className="flex-column">
-//               <Link to="/" className="nav-link" onClick={toggleMenu}>
-//                 <FontAwesomeIcon icon={faHome} className="mr-2" />
-//                 Dashboard
-//               </Link>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <FontAwesomeIcon icon={faUserGraduate} className="mr-2" />
-//                   Manage Session
-//                 </Nav.Link>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <FontAwesomeIcon icon={faUsers} className="mr-2" />
-//                   Students
-//                 </Nav.Link>
-//                 <ListGroup>
-//                   <Link to="/admit-student" className="list-group-item" onClick={toggleMenu}>
-//                     <FontAwesomeIcon icon={faUserGraduate} className="mr-2" />
-//                     Admit Student
-//                   </Link>
-//                   <Link to="/admit-bulk-student" className="list-group-item" onClick={toggleMenu}>
-//                     <FontAwesomeIcon icon={faUserGraduate} className="mr-2" />
-//                     Admit Bulk Student
-//                   </Link>
-//                   <Link to="/student-information" className="list-group-item" onClick={toggleMenu}>
-//                     <FontAwesomeIcon icon={faUserGraduate} className="mr-2" />
-//                     Student Information
-//                   </Link>
-//                 </ListGroup>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <FontAwesomeIcon icon={faLevelUpAlt} className="mr-2" />
-//                   Level
-//                 </Nav.Link>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <FontAwesomeIcon icon={faChalkboardTeacher} className="mr-2" />
-//                   Teachers
-//                 </Nav.Link>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <FontAwesomeIcon icon={faUsers} className="mr-2" />
-//                   Parents
-//                 </Nav.Link>
-//               </Nav.Item>
-//               <Nav.Item>
-//                 <Nav.Link onClick={toggleMenu}>
-//                   <Link to="librarians"><FontAwesomeIcon icon={faBook} className="mr-2" />
-//                   <span>Librarians</span></Link>
-//                 </Nav.Link>
-//               </Nav.Item>
-//               </Nav>
-//               </Col>
-//               <Col sm={10}></Col>
-//               </Row>
-//               </Container>
-              
-//   )}
-//   export default AdminSidebarMenu
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup,Nav } from 'react-bootstrap';
 
 const AdminSidebarMenu = () => {
   const [studentOpen, setStudentOpen] = useState(false);
@@ -91,6 +13,11 @@ const AdminSidebarMenu = () => {
   const toggleStudent = () => {
     setStudentOpen(!studentOpen);
   }
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
       <Container>
@@ -103,66 +30,114 @@ const AdminSidebarMenu = () => {
               <span></span>
             </div>
             <div className="sidebar-menu">
-              <ListGroup>
-                <ListGroup.Item>
-                  <Link to="/librarians">
-                    <FontAwesomeIcon icon={faBook} className="mr-2" />
-                    Librarians
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link to="/hostel">
-                    <FontAwesomeIcon icon={faHome} className="mr-2" />
-                    Hostel Manager
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link to="/clubs">
-                    <FontAwesomeIcon icon={faUsers} className="mr-2" />
-                    School Clubs
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link to="/exams">
-                    <FontAwesomeIcon icon={faClipboard} className="mr-2" />
-                    Exams
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link to="/subjects">
-                    <FontAwesomeIcon icon={faBookOpen} className="mr-2" />
-                    Subjects
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item onClick={toggleStudent}>
-                  <div className="d-flex align-items-center justify-content-between">
-                    <div>
-                      <FontAwesomeIcon icon={studentOpen ? faChevronDown : faChevronRight} className="mr-2" />
-                      Students
-                    </div>
-                    <div className="badge badge-pill badge-primary">3</div>
-                  </div>
-                  {studentOpen && (
+            <Nav className="flex-column align-items-start">
+               <Link to="/" className="nav-link" onClick={toggleMenu}>
+                 <FontAwesomeIcon icon={faHome} className="me-2" />
+                 Dashboard
+               </Link>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                   <FontAwesomeIcon icon={faUserGraduate} className="me-2" />
+                   Manage Session
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={(e)=>{
+                    e.stopPropagation()
+                    toggleStudent()
+                 }}>
+                   <FontAwesomeIcon icon={faUsers} className="me-2" />
+                   <span>
+
+                   Students
+                   </span>
+                   <FontAwesomeIcon icon={studentOpen ? faChevronDown : faChevronRight} className="ms-2" />
+                 </Nav.Link>
+                
+                   {studentOpen && (
                     <ListGroup variant="flush">
                       <ListGroup.Item>
-                        <Link to="/admit-student">Admit Student</Link>
+                        <Link to="/admit-student">
+                        <FontAwesomeIcon icon={faUserGraduate} className="me-2" />
+                            Admit Student</Link>
                       </ListGroup.Item>
                       <ListGroup.Item>
-                        <Link to="/admit-bulk">Admit Bulk Student</Link>
+                        <Link to="/admit-bulk"><FontAwesomeIcon icon={faUsers} className="me-2" />Admit Bulk Student</Link>
                       </ListGroup.Item>
                       <ListGroup.Item>
-                        <Link to="/student-info">Student Information</Link>
+                        <Link to="/student-info"> 
+                        <FontAwesomeIcon icon={faInfo} className="me-2" />
+                        Student Information</Link>
                       </ListGroup.Item>
                     </ListGroup>
                   )}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link to="/id-cards">
-                    <FontAwesomeIcon icon={faIdCard} className="mr-2" />
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                   <FontAwesomeIcon icon={faLevelUpAlt} className="me-2" />
+                   Level
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                   <FontAwesomeIcon icon={faChalkboardTeacher} className="me-2" />
+                   Teachers
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                   <FontAwesomeIcon icon={faUsers} className="me-2" />
+                   Parents
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                   <Link to="librarians"><FontAwesomeIcon icon={faBook} className="me-2" />
+                   <span>Librarians</span></Link>
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                 <Link to="/hostel">
+                    <FontAwesomeIcon icon={faHome} className="me-2" />
+                    Hostel Manager
+                  </Link>
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                 <Link to="/clubs">
+                    <FontAwesomeIcon icon={faUsers} className="me-2" />
+                    School Clubs
+                  </Link>
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                 <Link to="/exams">
+                    <FontAwesomeIcon icon={faClipboard} className="me-2" />
+                    Exams
+                  </Link>
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                 <Link to="/subjects">
+                    <FontAwesomeIcon icon={faBookOpen} className="me-2" />
+                    Subjects
+                  </Link>
+                 </Nav.Link>
+               </Nav.Item>
+               <Nav.Item>
+                 <Nav.Link onClick={toggleMenu}>
+                 <Link to="/id-cards">
+                    <FontAwesomeIcon icon={faIdCard} className="me-2" />
                     Generate ID Cards
                   </Link>
-                </ListGroup.Item>
-              </ListGroup>
+                 </Nav.Link>
+               </Nav.Item>
+               </Nav>
+              
             </div>
     </div>
           </Col>
