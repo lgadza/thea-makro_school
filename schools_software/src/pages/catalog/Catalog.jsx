e5e28a02f21008758fec3d8a04cbd83de803772c
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-04-27 12:18:11 +0200

    catalog good

--- schools_software/src/pages/catalog/Catalog.jsx
+++ schools_software/src/pages/catalog/Catalog.jsx
@@ -1,5 +1,5 @@
 
-import { faBars, faHome, faUserGraduate, faLevelUpAlt, faChalkboardTeacher, faUsers, faBook, faBed, faFileAlt,faInfo, faIdCard,faBookOpen,faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHome, faUserGraduate, faLevelUpAlt, faChalkboardTeacher, faUsers, faBook, faBed, faFileAlt,faInfo, faIdCard,faBookOpen,faClipboard, faMagnifyingGlass, faSitemap, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -39,166 +39,36 @@ const Catalog = ()=> {
             <Nav className="flex-column align-items-start">
            
                <Link to="/" className="nav-link" onClick={toggleMenu}>
-                 <FontAwesomeIcon icon={faHome} className="me-2" />
-                 Dashboard
+                 <FontAwesomeIcon icon={faUsers} className="me-2" />
+                 Students, Employees
                </Link>
                <Nav.Item>
                  <Nav.Link onClick={toggleMenu}>
-                   <FontAwesomeIcon icon={faUserGraduate} className="me-2" />
-                   Manage Session
+                   <FontAwesomeIcon icon={faSitemap} className="me-2" />
+                   Organizational units
                  </Nav.Link>
                </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={(e)=>{
-                    e.stopPropagation()
-                    toggleStudent()
-                 }}>
-                   <FontAwesomeIcon icon={faUsers} className="me-2" />
-                   <span>
-
-                   Students
-                   </span>
-                   <FontAwesomeIcon icon={studentOpen ? faChevronDown : faChevronRight} className="ms-2" />
-                 </Nav.Link>
-                
-                   {studentOpen && (
-                    <ListGroup variant="flush" className="flex-column align-items-start">
-                      <ListGroup.Item>
-                        <Link to="/admit-student">
-                        <FontAwesomeIcon icon={faUserGraduate} className="me-2" />
-                            Admit Student</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/admit-bulk" className='nowrap'><FontAwesomeIcon icon={faUsers} className="me-2" />Admit in-Bulk</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/student-info"> 
-                        <FontAwesomeIcon icon={faInfo} className="me-2" />
-                        Student Info</Link>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  )}
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleMenu}>
-                   <FontAwesomeIcon icon={faLevelUpAlt} className="me-2" />
-                   Level
-                 </Nav.Link>
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleMenu}>
-                   <FontAwesomeIcon icon={faChalkboardTeacher} className="me-2" />
-                   Teachers
-                 </Nav.Link>
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleMenu}>
-                   <FontAwesomeIcon icon={faUsers} className="me-2" />
-                   Parents
-                 </Nav.Link>
-               </Nav.Item>
+               
+             
                <Nav.Item>
                  <Nav.Link onClick={toggleMenu}>
                    <Link to="librarians"><FontAwesomeIcon icon={faBook} className="me-2" />
-                   <span>Librarians</span></Link>
+                   <span>Subjects</span></Link>
                  </Nav.Link>
                </Nav.Item>
                <Nav.Item>
                  <Nav.Link onClick={toggleMenu}>
                  <Link to="/hostel">
-                    <FontAwesomeIcon icon={faHome} className="me-2" />
-                    Hostel Manager
+                    <FontAwesomeIcon icon={faBookOpen} className="me-2" />
+                    Studies
                   </Link>
                  </Nav.Link>
                </Nav.Item>
                <Nav.Item>
                  <Nav.Link onClick={toggleMenu}>
                  <Link to="/clubs">
-                    <FontAwesomeIcon icon={faUsers} className="me-2" />
-                    School Clubs
-                  </Link>
-                 </Nav.Link>
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleMenu}>
-                 <Link to="/exams">
-                    <FontAwesomeIcon icon={faClipboard} className="me-2" />
-                    Exams
-                  </Link>
-                 </Nav.Link>
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleSubjects}>
-
-                    <FontAwesomeIcon icon={faBookOpen} className="me-2" />
-                    Subjects
-                   <FontAwesomeIcon icon={subjectsOpen ? faChevronDown : faChevronRight} className="ms-2" />
-
-            
-                 </Nav.Link>
-                 {subjectsOpen && (
-                    <ListGroup variant="flush" className="flex-column align-items-start">
-                      <ListGroup.Item>
-                        <Link to="/maths">
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                            Mathematics</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/chemistry"><FontAwesomeIcon icon={faBook} className="me-2" />Chemistry</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/biology"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Biology</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/physics"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Physics</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/history"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        History</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/geology"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Geology</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/agriculture"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Agriculture</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/tg"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        TG</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/fashion"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Fashion</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/woodwork"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Wood work</Link>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <Link to="/accounting"> 
-                        <FontAwesomeIcon icon={faBook} className="me-2" />
-                        Accounting</Link>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  )}
-               </Nav.Item>
-               <Nav.Item>
-                 <Nav.Link onClick={toggleMenu}>
-                 <Link to="/id-cards">
-                    <FontAwesomeIcon icon={faIdCard} className="me-2" />
-                    Generate ID Cards
+                    <FontAwesomeIcon icon={faQuestionCircle} className="me-2" />
+                    Help
                   </Link>
                  </Nav.Link>
                </Nav.Item>
@@ -213,9 +83,11 @@ const Catalog = ()=> {
 
     <div>
       <h3 className='text-start'>Catalog</h3>
-      <Card>
-        <CardHeader>
-          <h5 className='text-start'>People search</h5>
+      <Card className='my-3'>
+        <CardHeader className='d-flex align-items-center'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='mx-0 pe-2' />
+          <h5 className='text-start my-1 p-0'>
+            People search</h5>
         </CardHeader>
 
       <Form className='d-flex'>
@@ -224,9 +96,10 @@ const Catalog = ()=> {
         <Button variant="outline-primary">Search</Button>
       </Form>
       </Card>
-      <Card>
-        <CardHeader>
-          <h5 className='text-start'>Unit search</h5>
+      <Card className='my-3'>
+        <CardHeader className='d-flex align-items-center'>
+      <FontAwesomeIcon icon={faMagnifyingGlass} className='mx-0 pe-2' />
+          <h5 className='text-start my-1 p-0'>Unit search</h5>
         </CardHeader>
 
       <Form className='d-flex'>
@@ -235,9 +108,10 @@ const Catalog = ()=> {
         <Button variant="outline-primary">Search</Button>
       </Form>
       </Card>
-      <Card>
-        <CardHeader>
-          <h5 className='text-start'>Study search</h5>
+      <Card className='my-3'>
+        <CardHeader className='d-flex align-items-center'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='mx-0 pe-2' />
+          <h5 className='text-start my-1 p-0'>Study search</h5>
         </CardHeader>
 
       <Form className='d-flex'>
