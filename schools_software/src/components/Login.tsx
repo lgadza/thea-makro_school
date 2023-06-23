445e391daaaf08971b750f529851a2598c12e521
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-06-23 12:30:11 +0200

    logn in

---
+++ schools_software/src/components/Login.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Nav,Button,Form,Card, FormControl } from 'react-bootstrap';
+import fhs1 from '../assets/fhs_img/fhs1.jpg'
+import fhs_logo from "../assets/fhs_img/fhs_logo.png"
+import { Container } from 'react-bootstrap/lib/Tab';
+
+function Login(): JSX.Element {
+  return (
+    <Container>
+        <Card>
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Log in
+  </Button>
+</Form>
+        </Card>
+    </Container>
+  );
+}
+
+export default Login;
