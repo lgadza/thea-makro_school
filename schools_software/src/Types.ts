c6d0c5562da1c53661774a9e3654fccf9ae7ab19
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-07-04 23:15:16 +0200

    data collection

--- schools_software/src/Types.ts
+++ schools_software/src/Types.ts
@@ -6,3 +6,24 @@ export interface ImageProps {
   height: number;
 }
 
+export interface ApplicantRegistration{
+  first_name:string;
+  last_name:string;
+  second_name?:string;
+  date_of_birth:string;
+  gender:string;
+  citizenship:string;
+  street:string;
+  building_number:string;
+  apartment_number?:string;
+  postal_code?:string;
+  city:string;
+  province:string;
+  country:string;
+  phone_number:string;
+  email?:string;
+  policy_acceptance:string;
+  data_process_acceptance:string;
+  password:string;
+  settlement_type:string
+}
--- schools_software/src/pages/admissionsManagement/student/PersonalDataForm.tsx
+++ schools_software/src/pages/admissionsManagement/student/PersonalDataForm.tsx
@@ -1,16 +1,60 @@
 import { Col, Container, Row,Form, Button } from "react-bootstrap" 
 import "../styling.css"
+import { ApplicantRegistration } from "../../../Types"
+import { useState } from "react"
 const PersonalDataForm=():JSX.Element=>{
+
+
+const initialFormData: ApplicantRegistration = {
+  first_name: '',
+  last_name: '',
+  second_name: '',
+  date_of_birth: '',
+  gender: '',
+  citizenship: '',
+  street: '',
+  building_number: '',
+  apartment_number: '',
+  postal_code: '',
+  city: '',
+  province: '',
+  country: '',
+  phone_number: '',
+  email: '',
+  policy_acceptance: '',
+  data_process_acceptance: '',
+  password: '',
+  settlement_type: '',
+};
+
+const [formData, setFormData] = useState<ApplicantRegistration>(initialFormData);
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+  console.log(formData);
+};
+const handleChange=(e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>{
+  const {name,value}=e.target;
+setFormData((prevFormData)=>({
+  ...prevFormData,
+  [name]:value
+}))
+}
    return( 
     <Container>
     {/* Student Personal Data*/}
 
         <h5 className="d-flex mb-4 header">Personal data</h5>
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">First name <span className="text-danger">*</span></Form.Label>
-          <Form.Control placeholder="First name" required/>
+          <Form.Control
+           placeholder="First name"
+           name="first_name"
+           value={formData.first_name}
+            required 
+            onChange={handleChange}
+            />
         </Col>
         <Col>
         <Form.Label className="d-flex">Last name <span className="text-danger">*</span></Form.Label>
@@ -18,7 +62,7 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">Second name </Form.Label>
@@ -30,7 +74,7 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">Citizenship<span className="text-danger">*</span></Form.Label>
@@ -47,21 +91,25 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
+        <Col>
+        <Form.Label className="d-flex">Code<span className="text-danger">*</span></Form.Label>
+          <Form.Control  as="select" required>
+          <option value="236">263</option>
+          <option value="27">27</option>
+          </Form.Control>
+        </Col>
         <Col>
         <Form.Label className="d-flex">Mobile number<span className="text-danger">*</span></Form.Label>
           <Form.Control type="tel" placeholder="Phone number" required/>
         </Col>
-        <Col>
        
-  
-        </Col>
       </Row>
     </Form >
     {/* Student Address */}
     <h5 className="d-flex my-4 header">Address</h5>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">Street <span className="text-danger">*</span></Form.Label>
@@ -73,7 +121,7 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">Apartment no </Form.Label>
@@ -85,7 +133,7 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">City<span className="text-danger">*</span></Form.Label>
@@ -105,33 +153,38 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
+      <Col>
+        <Form.Label className="d-flex">Province<span className="text-danger">*</span></Form.Label>
+          <Form.Control as="select" placeholder="Phone number" required>
+            <option value="city">Bulawayo</option>
+            <option value="village">Harare</option>
+          </Form.Control>
+  
+        </Col>
         <Col>
-        <Form.Label className="d-flex">Type of living place<span className="text-danger">*</span></Form.Label>
+        <Form.Label className="d-flex">Type of settlement<span className="text-danger">*</span></Form.Label>
           <Form.Control as="select" placeholder="Phone number" required>
             <option value="city">City</option>
             <option value="village">Village</option>
           </Form.Control>
         </Col>
-        <Col>
-       
-  
-        </Col>
+     
       </Row>
     </Form>
     {/* Login details */}
     <h5 className="d-flex my-4 header">Login details</h5>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
-        <Form.Label className="d-flex">Email <span className="text-danger">*</span></Form.Label>
-          <Form.Control placeholder="Email" required/>
+        <Form.Label className="d-flex">Email </Form.Label>
+          <Form.Control placeholder="Email" />
         </Col>
        
       </Row>
     </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Form.Label className="d-flex">Password <span className="text-danger">*</span></Form.Label>
@@ -143,7 +196,7 @@ const PersonalDataForm=():JSX.Element=>{
         </Col>
       </Row>
     </Form>
- <Form className="my-3">
+ <Form className="my-3" onSubmit={handleSubmit}>
   <Row>
     <Col>
     <Form.Check  type="checkbox" id="checkbox" label="I confirm that I have read the Regulation and Privacy Policy and I declare that I accept them."/>
@@ -152,7 +205,7 @@ const PersonalDataForm=():JSX.Element=>{
   </Row>
 
  </Form>
-    <Form className="my-3">
+    <Form className="my-3" onSubmit={handleSubmit}>
       <Row>
         <Col>
         <Button variant="primary" className="w-100 mt-3 justify-content-end" type="submit">
