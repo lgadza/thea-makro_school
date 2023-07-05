01d872ba398aab575d3a6e42829f8769cb0c1274
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-07-05 14:46:59 +0200

    good

--- schools_software/src/pages/admissionsManagement/student/ApplicationPage.tsx
+++ schools_software/src/pages/admissionsManagement/student/ApplicationPage.tsx
@@ -8,13 +8,7 @@ const ApplicationPage=():JSX.Element=>{
     <Container className="application_page content_bg">
         <Row>
             <Col className="d-flex banner_container px-0 mb-4">
-               
-            {/* <Image src={image} height={100} width={100} alt="userName"/>
-
-                </div>
-            <div><h6>Student name</h6>
-            <div>7904334452</div>
-            <span>Level</span> */}
+         
             <img src={image} className="banner" />
            <h6 className="banner_label">Makrodex Tech Pvt Ltd</h6>
             </Col>
--- schools_software/src/pages/admissionsManagement/student/account/StudentAccountPage.tsx
+++ schools_software/src/pages/admissionsManagement/student/account/StudentAccountPage.tsx
@@ -1,15 +1,30 @@
 import { Col, Container,Row } from "react-bootstrap"
 import StudentNavigationbar from "../StudentNavigationbar"
 import PersonalData from "./PersonalData"
+import Image from "../../../../components/Image"
+import image from "../../../../assets/fhs_img/fhs1.jpg"
 
 const StudentAccountPage=():JSX.Element=>{
     return(
-        <Container  className="content_bg">
+        <Container >
+        <Row>
+            <Col className="d-flex py-3 content_bg  my-4 student_account_border">
+               
+            <Image src={image} height={100} width={100} alt="userName"/>
+<div className="d-flex flex-column align-items-start px-3">
+    <h5>Student Name</h5>
+<div>Phone number</div>
+<div>Form 4</div>
+</div>
+          
+            </Col>
+            
+        </Row>
 <Row>
-    <Col sm={3}>
+    <Col sm={3} className="student_account_border content_bg py-3 ">
     <StudentNavigationbar/>
     </Col>
-    <Col sm={9}>
+    <Col sm={9} className="student_account_border content_bg py-3 ">
     <PersonalData/>
     </Col>
 </Row>
