5e883324ffd055e1d981dfa0dfd1dd8ac38a165e
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-08-07 04:01:23 +0200

    better thinngs

--- schools_software/src/pages/Home/Simplifyprocess.tsx
+++ schools_software/src/pages/Home/Simplifyprocess.tsx
@@ -1,122 +1,131 @@
-import { useEffect, useState } from "react";
-import { admissionFeatures } from "../../assets/data/makrohome"
-import { PushButton } from "../../components/Buttons";
-const SimplifyProcess:React.FC=()=>{
-    const [currentFeature, setCurrentFeature] = useState<number>(1);
 
-    const featureSwitcher = () => {
-      setCurrentFeature((prevFeature) => (prevFeature % 3) + 1);
-    };
-  
-    useEffect(() => {
-      const featureTimer = setInterval(featureSwitcher, 5000);
-      return () => clearInterval(featureTimer);
-    }, []);
-  
-    const handleFeatureClick = (featureNumber: number) => {
-      setCurrentFeature(featureNumber);
-    };
-  
-interface AdmissionCardProps {
-  title: string;
-  content: string;
+import React, { useEffect, useState } from 'react';
+import { admissionFeatures } from '../../assets/data/makrohome';
+import { PushButton } from '../../components/Buttons';
+import img1 from "../../assets/imgs/candidateAccount.png";
+import img2 from "../../assets/candidatesAdmission.png";
+import img3 from "../../assets/candidatesAdmission2.png";
+import "./MakroHomePage.css"; 
+interface AdmissionCardData {
+  id: number;
+  imgUrl: string;
 }
 
-const AdmissionCard: React.FC<AdmissionCardProps> = ({ title, content }) => {
-  return (
-    <li className="card">
-      <h1>{title}</h1>
-      <p>{content}</p>
-    </li>
-  );
-};
+interface AdmissionCardProps extends AdmissionCardData {
+  className: string;
+}
 
-const AdmissionCards: React.FC = () => {
-  const admissionData: AdmissionCardProps[] = [
-    {
-      title: 'Admission Card 1',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum, lorem vel tincidunt...',
-    },
-    {
-      title: 'Admission Card 2',
-      content:
-        'Vestibulum nunc massa, gravida quis porta nec, feugiat id metus. Nunc ac arcu dolor...',
-    },
-    {
-      title: 'Admission Card 3',
-      content:
-        'Donec nunc ligula, vulputate quis mollis eu, interdum quis libero. Donec nulla ante...',
-    },
-    {
-      title: 'Admission Card 4',
-      content:
-        'Donec nunc ligula, vulputate quis mollis eu, interdum quis libero. Donec nulla ante...',
-    },
-  ];
+const admissionData: AdmissionCardData[] = [
+  {
+    id: 1,
+    imgUrl: img1, 
+  },
+  {
+    id: 2,
+    imgUrl: img2, 
+  },
+  {
+    id: 3,
+    imgUrl: img3, 
+  },
+];
 
-  const [currentCard, setCurrentCard] = useState(0);
+const SimplifyProcess: React.FC = () => {
+  const [currentCard, setCurrentCard] = useState(1);
+  const [previousCard, setPreviousCard] = useState(0);
+  const [currentFeature, setCurrentFeature] = useState<number>(1);
 
   const handleCardClick = (index: number) => {
     setCurrentCard(index);
   };
 
+  const handleNextCard = () => {
+    const nextCard = currentCard % 3 + 1;
+    setCurrentCard(nextCard);
+    setPreviousCard(currentCard);
+    setCurrentFeature((prevFeature) => (prevFeature % 3) + 1);
+  };
+
+  useEffect(() => {
+    const featureTimer = setInterval(handleNextCard, 5000);
+    return () => clearInterval(featureTimer);
+  }, [currentCard]);
+
+  const AdmissionCard: React.FC<AdmissionCardProps> = ({ id, imgUrl }) => {
+    return (
+      <li
+        className={`admission-card ${
+          id === currentCard ? "card--current" : id === previousCard ? "card--out" : ""
+        }`}
+        onClick={() => handleCardClick(id)}
+      >
+        <img src={imgUrl} alt={`img${id}`} style={{ width: "100%", height: "300px" }} />
+      </li>
+    );
+  };
+
   return (
-    <ul className="cards">
-      {admissionData.map((data, index) => (
-        <AdmissionCard
-          key={index}
-          title={data.title}
-          content={data.content}
-          onClick={() => handleCardClick(index)} 
-          className={index === currentCard ? 'card--current' : ''}
-        />
-      ))}
-    </ul>
+    <div className="p-3 simplify-feature admission-card-bg">
+       <input type="radio" id="feature1" className="sec-1-input" name="feature" checked={currentFeature === 1} />
+      <input type="radio" id="feature2" className="sec-1-input" name="feature" checked={currentFeature === 2} />
+      <input type="radio" id="feature3" className="sec-1-input" name="feature" checked={currentFeature === 3} />
+      <div className="row my-4 me-2">
+        <div className="col">
+          <h3 className="text-start header my-3">Admission Management</h3>
+          <h4 className="text-start">
+            Elevate the admissions process to deliver an enriching experience for all
+          </h4>
+          <ul>
+            {admissionFeatures.map((feature) => (
+              <li
+                style={{
+                  color: currentCard === feature.id ? '' : 'gray',
+                }}
+                className="text-start py-4"
+                key={feature.id}
+              >
+                <div className="d-flex flex-column">
+                  <span className="my-3 simplify-feature-text">{feature.heading} </span>
+                  {currentCard === feature.id && (
+                    <small className="simplify-feature-text">{feature.content} </small>
+                  )}
+                </div>
+                <span
+                  style={{
+                    backgroundColor: currentCard === feature.id ? 'white' : 'gray',
+                  }}
+                  className="simplify-progressbar"
+                >
+                  <span className="simplify-progressbar-fill"></span>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col better-for-card">
+          <div className="slider admission-card-bg">
+            <ul className="cards">
+              {admissionData.map((data) => (
+                <AdmissionCard
+                  key={data.id}
+                  id={data.id}
+                  imgUrl={data.imgUrl}
+                  className={
+                    data.id === currentCard ? "card--current" : data.id === previousCard ? "card--out" : ""
+                  }
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="text-start">
+
+        <PushButton message="Learn more!" />
+
+      </div>
+    </div>
   );
 };
 
-
-    return(
-        <div className="p-3 simplify-feature">
-             <input type="radio" id="feature1" className="sec-1-input" name="feature" checked={currentFeature === 1} />
-        <input type="radio" id="feature2" className="sec-1-input" name="feature" checked={currentFeature === 2} />
-        <input type="radio" id="feature3" className="sec-1-input" name="feature" checked={currentFeature === 3} />
-<div className="row">
-    <div className="col">
-<h3 className="text-start header" >Admission Management</h3>
-<h4 className="text-start">Elevate the admissions process to deliver an enriching experience for all</h4>
-        <ul>
-        {admissionFeatures.map((feature,_)=>{
-           return <li 
-           style={{
-            color: currentFeature === feature.id ? "" : "gray",
-          }} 
-          className="text-start py-4" key={feature.id}>
-                    <div className="d-flex flex-column">
-                        <span className="my-3 simplify-feature-text">{feature.heading} </span> 
-                        {currentFeature === feature.id && (<small className="simplify-feature-text">{feature.content} </small>)}   
-                            
-                    </div>
-                    <span   style={{
-backgroundColor: currentFeature === feature.id ? "white" : "gray",
-          }}  className="simplify-progressbar">
-                         <span className="simplify-progressbar-fill"></span>
-                    </span>
-            </li>
-        })}
-        </ul>
-    </div>
-    <div className="col">
-        <div className="slider">
-        </div>
-    </div>
-</div>
-<div className="text-start">
-<PushButton message="Learn more!"/>
-</div>
-
-        </div>
-    )
-}
-export default SimplifyProcess
+export default SimplifyProcess;
