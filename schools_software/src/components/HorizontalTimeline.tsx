bc5fb96e63cba2ca7dcd2900d7f8c411a4b498d3
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-08-08 11:31:03 +0200

    horozontal timeline

--- schools_software/src/components/HorizontalTimeline.tsx
+++ schools_software/src/components/HorizontalTimeline.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './HorizontalTimeline.css'; 
 import calaUpload from "../assets/CALAUpload.png"
+import textExtration from "../assets/textExtraction2.png"
+import contentCategory from "../assets/contentCategory.png"
 import * as Icon from "react-bootstrap-icons"
 const HorizontalTimeline: React.FC = () => {
   return (
@@ -15,7 +17,7 @@ const HorizontalTimeline: React.FC = () => {
         <div className="row">
           <div className="steps-timeline text-center">
             <div className="steps-one">
-              <h5>Upload your PDFs</h5>
+              <h5>Upload Your Files</h5>
               <div className="end-circle back-orange"></div>
               <div className="step-wrap">
                 <div className="steps-stops">
@@ -31,13 +33,13 @@ const HorizontalTimeline: React.FC = () => {
                 </div>
               </div>
               <div className="inverted-pane-warp back-blue">
-                <div className="inverted-steps-pane">
+                <div className="ps-2 inverted-steps-pane text-start">
                   <small>Upload multiple PDF files or any kind of text file directly from your device</small>
                 </div>
               </div>
             </div>
             <div className="steps-one">
-              <h5>Upload your PDFs</h5>
+              <h5>Text Extraction</h5>
               <div className="end-circle back-orange"></div>
               <div className="step-wrap">
                 <div className="steps-stops">
@@ -46,20 +48,20 @@ const HorizontalTimeline: React.FC = () => {
               </div>
               <div className="pane-warp back-blue">
                 <div className="steps-pane">
-                  <img src={calaUpload} alt="CalaUpload" />
-                  <div className='Icon' >
-                  <Icon.FilePdf size={30} color="red"/>
+                  <img src={textExtration} alt="textExtration" />
+                  <div className='Icon fileExe'  >
+                  <Icon.FiletypeExe size={30} />
                   </div>
                 </div>
               </div>
               <div className="inverted-pane-warp back-blue">
-                <div className="inverted-steps-pane">
-                  <small>Upload multiple PDF files or any kind of text file directly from your device</small>
+                <div className="ps-2 inverted-steps-pane text-start">
+                  <small>Extract text and data from uploaded files through advanced parsing.</small>
                 </div>
               </div>
             </div>
             <div className="steps-one">
-              <h5>Upload your PDFs</h5>
+              <h5>Content Categorization</h5>
               <div className="end-circle back-orange"></div>
               <div className="step-wrap">
                 <div className="steps-stops">
@@ -68,20 +70,20 @@ const HorizontalTimeline: React.FC = () => {
               </div>
               <div className="pane-warp back-blue">
                 <div className="steps-pane">
-                  <img src={calaUpload} alt="CalaUpload" />
-                  <div className='Icon' >
-                  <Icon.FilePdf size={30} color="red"/>
+                  <img src={contentCategory} alt="ContentCategory" />
+                  <div className='Icon bookmark' >
+                  <Icon.Bookmarks size={30} color="rgb(2, 130, 136)"/>
                   </div>
                 </div>
               </div>
               <div className="inverted-pane-warp back-blue">
-                <div className="inverted-steps-pane">
-                  <small>Upload multiple PDF files or any kind of text file directly from your device</small>
+                <div className="ps-2 inverted-steps-pane text-start">
+                  <small>Categorize and organize extracted content by subject and type for efficient access.</small>
                 </div>
               </div>
             </div>
             <div className="steps-one">
-              <h5>Upload your PDFs</h5>
+              <h5>Search Interface</h5>
               <div className="end-circle back-orange"></div>
               <div className="step-wrap">
                 <div className="steps-stops">
@@ -97,13 +99,13 @@ const HorizontalTimeline: React.FC = () => {
                 </div>
               </div>
               <div className="inverted-pane-warp back-blue">
-                <div className="inverted-steps-pane">
-                  <small>Upload multiple PDF files or any kind of text file directly from your device</small>
+                <div className="ps-2 inverted-steps-pane text-start">
+                  <small>Query the AI using natural language to find specific information from categorized content.</small>
                 </div>
               </div>
             </div>
             <div className="steps-one">
-              <h5>Upload your PDFs</h5>
+              <h5>Personalized Insights</h5>
               <div className="end-circle back-orange"></div>
               <div className="step-wrap">
                 <div className="steps-stops">
@@ -119,8 +121,8 @@ const HorizontalTimeline: React.FC = () => {
                 </div>
               </div>
               <div className="inverted-pane-warp back-blue">
-                <div className="inverted-steps-pane">
-                  <small>Upload multiple PDF files or any kind of text file directly from your device</small>
+                <div className="ps-2 inverted-steps-pane text-start">
+                  <small>Get tailored recommendations and insights based on your queries and preferences.</small>
                 </div>
               </div>
             </div>
