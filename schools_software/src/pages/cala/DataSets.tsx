89bd0010c6637ff87b4f9dd4b0e2bf9d360272a6
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-09-14 22:29:52 +0200

    Update DataSets.tsx

--- schools_software/src/pages/cala/DataSets.tsx
+++ schools_software/src/pages/cala/DataSets.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDatabase, faComment,faTrashCan, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Container, Row, Col, Button } from 'react-bootstrap';
