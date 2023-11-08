70aa633ad67d931b78bac86d65529e763c413c87
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-11-08 17:02:58 +0100

    qs

--- schools_software/src/pages/Home/FAQComp.tsx
+++ schools_software/src/pages/Home/FAQComp.tsx
@@ -22,7 +22,7 @@ const FAQComp: React.FC<FAQCompoProps> = ({FAQItems}) => {
           <div className="FAQ-item" key={item.id}>
             <button
               id={`FAQ-button-${item.id}`}
-              className='d-flex align-items-center'
+              className='d-flex align-items-center py-2'
               aria-expanded={expandedId === item.id ? 'true' : 'false'}
               onClick={() => toggleFAQ(item.id)}
             >
